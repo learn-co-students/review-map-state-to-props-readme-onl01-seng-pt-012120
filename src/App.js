3ccfd3bd9ff84b9e6298016276a9ed76a9021ffc
d-otis <dan@dan-foley.com>
2020-10-08 11:19:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.store.dispatch({
+    // this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.store.dispatch({
+    // this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
@@ -27,15 +29,28 @@ class App extends Component {
           <button onClick={() => this.handleOnClickUsers()}>
             Click to change user count
           </button>
-          <p>{this.props.items.length}</p>
+          <p>items count {this.props.items.length}</p>
+          {/*<p>users count {this.props.users.length}</p>*/}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+const mapStateToProps = state => {
+  // debugger;
+  return { items: state.items, users: state.users }
+  // return { orangePeel: ['a', 'b', 'c'] }
 }
 
 export default connect(mapStateToProps)(App);
+// connects to our store that's set up in our shoppingListItemReducer
+// passes the state as an argument into mapStateToProps so that an object
+// can be returned that's formatted correctly with the data we want to 
+// expose to the App component 
+
+// const vanilla = milkshake => {
+//   debugger;
+//   return { items: milkshake.items }
+// }
+
+// export default connect(vanilla)(App)
